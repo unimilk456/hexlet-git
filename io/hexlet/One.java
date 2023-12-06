30228e21aaa913f39253a7a490f39689a26b44da
Mikhail-Davidovich <mikhail.davidovich@edetek.com>
2023-12-06 23:01:05 +0400

    Added apache.lang

--- io/hexlet/One.java
+++ io/hexlet/One.java
@@ -1,7 +1,11 @@
 package io.hexlet;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class One {
     public static void main(String[] args) {
-        Two.greeting();
+	var capitalizedWord = StringUtils.reverse("hexlet");
+        System.out.println(capitalizedWord);		
+//        Two.greeting();
     }
 }
